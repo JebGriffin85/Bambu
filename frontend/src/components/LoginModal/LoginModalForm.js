8b195b30ce396e01c5c22891ee15bfbddbd88291
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-07-22 20:15:03 -0500

    login work

--- frontend/src/components/LoginModal/LoginModalForm.js
+++ frontend/src/components/LoginModal/LoginModalForm.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-
-
+import { Redirect, Link } from 'react-router-dom';
 
 function LoginFormPage({setOpen}) {
     const dispatch = useDispatch();
@@ -28,11 +26,15 @@ function LoginFormPage({setOpen}) {
             });
     }
 
+    const handleDemo = () => {
+        return dispatch(sessionActions.login({ credential: 'demo@demo.io', password:'password' }))
+    }
+
     return (
-        <div class="w-full max-w-xs">
+        <div className="w-full max-w-xs">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-0" onSubmit={handleSubmit}>
                 <ul>
-                    {errors.map((error, idx) => <li className='text-red-600' key={idx}>{error}</li>)}
+                    {errors.map((error, idx) => <li className='m-0 p-0 pt-4 text-sm text-red-600' key={idx}>{error}</li>)}
                 </ul>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -45,21 +47,30 @@ function LoginFormPage({setOpen}) {
                         required
                     />
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
-                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                 </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-100 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                <div className="flex flex-col items-center justify-center">
+                    <button className=" bg-blue-100 hover:bg-blue-200 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit">Log In</button>
                 </div>
+                <div className='text-center pt-3 content-center justify-center flex-col '>
+                    <p>Don't have an account?</p>
+                    <p onClick={handleDemo} className=" cursor-pointer bg-blue-100 hover:bg-blue-200 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                       >Demo Log In</p>
+                       <div className='pt-3'>
+                    <Link onClick={() => setOpen(false)} to='/signup' className="cursor-pointer bg-blue-100 hover:bg-blue-200 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >Sign Up</Link>
+                    </div>
+            </div>
             </form>
         </div>
     );
