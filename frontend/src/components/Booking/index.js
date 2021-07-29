d8b421efddda0a92d99e16ea35af146e20182e11
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-07-29 10:04:53 -0500

    date logic

--- frontend/src/components/Booking/index.js
+++ frontend/src/components/Booking/index.js
@@ -13,32 +13,38 @@ export default function Booking () {
     const maxDate = moment(new Date()).add(3, 'month')._d;
     const [date, setDate] = useState(moment(new Date()).add(1, 'day')._d);
     const [time, setTime] = useState(7);
-    const [tableId, setTableId] = useState(4);
+    const [tableId, setTableId] = useState(null);
+    const [btnDisable, setBtnDisable] = useState(false)
     const allRes = useSelector((state) => state.reservations.allReservations);
     const userId = useSelector((state) => state.session.user?.id);
+    const user = useSelector((state) => state.session.user);
     let availableTables = getAvailableTables(allRes);
     
     const updateTableId = (e) => {
-        setTableId(e.target.value)
+        setTableId(e.target.value);
     };
 
- console.log(availableTables)
- console.log(tableId)
-
-let valueMap = {
-    1: 'Table for One',
-    2: 'Table for Two',
-    3: 'Table for Two',
-    4: 'Table for Four',
-    5: 'Table for four',
-    6: 'Table for Six',
-    7: 'Table for Six'
-}
-
+    const updateTime = (e) => {
+        setTime(e.target.value);
+        console.log(parseInt(e.target.value))
+    };
+    
+    let valueMap = {
+        1: 'Table for One',
+        2: 'Table for Two',
+        3: 'Table for Two',
+        4: 'Table for Four',
+        5: 'Table for four',
+        6: 'Table for Six',
+        7: 'Table for Six'
+    }
+    
     useEffect(() => {
         dispatch(thunk_getAllRes(moment(minDate).startOf('day')._d))
+        if (user) setBtnDisable(user.hasReservation)
+        
     },[dispatch])
-
+    
     function handleClick (date) {
         dispatch(thunk_getAllRes(date));
     };
@@ -51,9 +57,10 @@ let valueMap = {
             userId,
             tableId
         };
-       await dispatch(thunk_flipReservation(userId))
+       await dispatch(thunk_flipReservation(userId));
        await dispatch(thunk_addRes(reservation));
-       await dispatch(thunk_getAllRes(newDate))
+       await dispatch(thunk_getAllRes(newDate));
+       setBtnDisable(true)
     };
 
     return (
@@ -70,13 +77,19 @@ let valueMap = {
                     <div>{reservation.date} number of seats: {reservation.Table.numofseats}</div>
                 )
             })}
-            <button onClick={handleAddReservation} >Add Reservation</button>
+            <button disabled={btnDisable} onClick={handleAddReservation} >Add Reservation</button>
             <p>tables available: </p>
             <select onChange={updateTableId}>
                 {availableTables.map((table => 
                 
                 <option value={table}>{valueMap[table]}</option>
                 ))}
+            </select>
+            <p>times available: </p>
+            <select onChange={updateTime} >
+                <option value='6'> 6:00 pm </option>
+                <option value='7'> 7:00 pm </option>
+                <option value='8'> 8:00 pm </option>
             </select>
         </div>
     )
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -56,6 +56,12 @@ function Navbar() {
                                         className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                                     > <LoginModal />
                                     </p>}
+                                    {sessionUser?.hasReservation && 
+                                    <NavLink to='my-reservation'
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        My Reservation
+                                    </NavLink>
+                                    }
                                 </div>
                             </div>
                         </div>
