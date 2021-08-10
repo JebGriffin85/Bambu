eafe4cd146583b2065dbfa904bb32ae8392a8d77
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-08-10 09:24:55 -0500

    signup done

--- frontend/src/components/FAQ/faq.js
+++ frontend/src/components/FAQ/faq.js
@@ -11,81 +11,67 @@ export default function FAQ () {
         <div class="container px-5 py-24 mx-auto">
           <div class="text-center mb-20">
             <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
-              Frequently Asked Question
+              Frequently Asked Questions
             </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-              The most common questions about how our business works and what
-              can do for you.
-            </p>
+      
           </div>
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
                 <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How Long is this site live?
+                    Are guests required to wear masks at Bambù?
                 </summary>
 
                 <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    Bambù updates our safety policies to reflect the most recent guidance from CDC and the City of Chicago on Covid19 protocols.
+                    Effective starting Wednesday, August 4th, 2021 all guests, regardless of vaccination status, are required to wear masks when not seated at their table. Vaccinated guests are permitted to remove their masks once seated, but will be asked to wear a mask when entering, exiting, and walking in the restaurant. Unvaccinated guests are required to wear their masks any time they are not actively eating or drinking.
                 </span>
               </details>
               <details class="mb-4">
                 <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                  Can I install/upload anything I want on there?
+                    Are you able to accommodate dietary restrictions?
                 </summary>
 
                 <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    At Bambù, we try to make the fewest number of changes possible when accommodating dietary restrictions, so that our guests may enjoy the menu as Chef Achatz originally envisioned. We are not able to make substitutions for aversions, as this will compromise the integrity of the experience. We are able to make accommodations for certain common dietary restrictions (Ovo-Lacto Vegetarian, Pescetarian, No shellfish or Finfish, No Nuts, No Gluten) with at least 72 hours advance notice.
                 </span>
               </details>
               <details class="">
                 <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How can I migrate to another site?
+                    What happens if I am running late?
                 </summary>
 
                 <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    Contact host@bambu.com if you are running late. Please note that arriving more than ten minutes late may result in missing part of your experience.
                 </span>
               </details>
             </div>
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
                 <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Can I change the domain you give me?
+                    Can I reschedule my booking?
                 </summary>
 
                 <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    We are able to reschedule your booking with at least 72 hours’ advance notice. After that time we are unable to accommodate changes.
                 </span>
               </details>
               <details class="mb-4">
                 <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How many sites I can create at once?
+                    What is the dress code at Bambù?
                 </summary>
 
                 <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    Our dress code is “elevated casual” - we only ask guests to be comfortable yet polished. Jackets are not required, though many guests do opt to wear them.
                 </span>
               </details>
               <details class="mb-4">
                 <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How can I communicate with you?
+                    What is your corkage policy?
                 </summary>
 
                 <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                    Per the policy set in place by the Chef and Owner, we do not allow any outside wine. We do, however, offer carefully selected and curated wine pairings to accompany each menu. In addition, we offer an extensive a la carte selection of wine by the bottle, and a limited selection of beer and sake.
                 </span>
               </details>
             </div>
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import signup from './signup.jpeg';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -28,48 +29,112 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    // <form onSubmit={handleSubmit}>
+      // <ul>
+      //   {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      // </ul>
+    //   <label>
+    //     Email
+    //     <input
+          // type="text"
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          // required
+    //     />
+    //   </label>
+    //   <label>
+    //     Username
+    //     <input
+    //       type="text"
+    //       value={username}
+    //       onChange={(e) => setUsername(e.target.value)}
+    //       required
+    //     />
+    //   </label>
+    //   <label>
+    //     Password
+    //     <input
+    //       type="password"
+    //       value={password}
+    //       onChange={(e) => setPassword(e.target.value)}
+    //       required
+    //     />
+    //   </label>
+    //   <label>
+    //     Confirm Password
+    //     <input
+    //       type="password"
+    //       value={confirmPassword}
+    //       onChange={(e) => setConfirmPassword(e.target.value)}
+    //       required
+    //     />
+    //   </label>
+    //   <button type="submit">Sign Up</button>
+    // </form>
+
+
+
+
+    <div class="flex min-h-screen bg-white">
+
+        <div class="w-1/2 bg-cover md:block hidden" style={{backgroundImage:  `url(${signup})`}}></div>
+
+        <div class="md:w-1/2 max-w-lg mx-auto my-24 px-4 py-5 shadow-none">
+
+            <div class="text-left p-0 font-sans">
+                
+                <h1 class=" text-gray-800 text-3xl font-medium">Create an account for free</h1>
+                <h3 class="p-1 text-gray-700">Creating an account will allow you to make reservations online</h3>
+            </div>
+            <form onSubmit={handleSubmit} class="p-0">
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+                <div class="mt-5">
+
+                 
+                    <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+                  class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="Email"/>
+                </div>
+                <div class="mt-5">
+                    <input 
+              type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                    class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="Username"/>
+                </div>
+                <div class="mt-5">
+                    <input 
+                     type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                    class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent  " placeholder="Password"/>
+                </div>
+          <div class="mt-5">
+            <input
+              type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+              class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent  " placeholder="Confirm Password" />
+          </div>
+
+                <div class="mt-10">
+                    <input type="submit" value="Sign up" class="cursor-pointer py-3 bg-green-500 text-white w-full rounded hover:bg-green-600"/>
+                </div>
+            </form>
+           
+        </div>
+
+
+    </div>
+
   );
 }
 
